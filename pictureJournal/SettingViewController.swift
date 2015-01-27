780d3406103ad333689f23c2b92696ad1e80b708
susien <tan@snv-loan99-lm.corp.yahoo.com>
2015-01-26 23:32:32 -0800

    My Picture Journal

---
+++ pictureJournal/SettingViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingViewController.swift
+//  pictureJournal
+//
+//  Created by Suzanne Tan on 1/26/15.
+//  Copyright (c) 2015 Suzanne Tan. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: NSObject {
+   
+}
